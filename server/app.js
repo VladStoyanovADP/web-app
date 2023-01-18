9e425269434444fd45fdc799632a1ec503c636f3
Vlad <vladstoyanoff@gmail.com>
2023-01-18 19:10:31 +0000

    .

--- server/app.js
+++ server/app.js
@@ -13,27 +13,26 @@ let fileName = undefined
 
 app.get('/', (req, res) =>
 {
-  ffmpeg(`./uploads/${fileName}`)
-    .setStartTime(`00:00:04`)
-    .setDuration("10")
-    .output("video_out.mp4")
-    .on("end", () => {
-      res.download("./video_out.mp4");
-    })
-    .run();
-  
-  // axios
-  //   .get("http://localhost:8000/test")
-  //   .then((resp) => resp.data[0].mostReplayed)
-  //   .then((startTime) => {
-  //     startTime /= 1000;
-  //     startTime = Math.floor(startTime);
-  //     let min = (startTime - (startTime % 60)) / 60;
-  //     if (min < 10) min = `0${min}`;
-  //     let seconds = startTime % 60;
-  //     if (seconds < 10) seconds = `0${seconds}`;
+  axios
+    .get("http://localhost:8000/test")
+    .then((resp) => resp.data[0].mostReplayed)
+    .then((startTime) => {
+      startTime /= 1000;
+      startTime = Math.floor(startTime);
+      let min = (startTime - (startTime % 60)) / 60;
+      if (min < 10) min = `0${min}`;
+      let seconds = startTime % 60;
+      if (seconds < 10) seconds = `0${seconds}`;
 
-
+      ffmpeg(`./uploads/${fileName}`)
+        .setStartTime(`00:${min}:${seconds}`)
+        .setDuration("10")
+        .output("video_out.mp4")
+        .on("end", () => {
+          res.download("./video_out.mp4");
+        })
+        .run();
+    });
 })
 
 const multer = require("multer");
